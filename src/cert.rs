a0577b5fe1944df84974651bc0bf3e28a82317b7
fox0 <red-fox0@yandex.ru>
2022-06-12 14:02:32 +0700

    up

--- src/cert.rs
+++ src/cert.rs
@@ -3,9 +3,20 @@ use std::os::raw::{c_uint, c_void};
 
 use crate::cprocsp::{
     CertCloseStore, CertFindCertificateInStore, CertOpenSystemStoreA, CryptStringToBinaryA,
-    CERT_NAME_STR_CRLF_FLAG, CERT_X500_NAME_STR, CRYPT_STRING_HEX, HCERTSTORE, PKCS_7_ASN_ENCODING,
-    X509_ASN_ENCODING,
+    CERT_FIND_HASH, CERT_NAME_STR_CRLF_FLAG, CERT_X500_NAME_STR, CRYPT_STRING_HEX, HCERTSTORE,
+    PKCS_7_ASN_ENCODING, X509_ASN_ENCODING, _CRYPTOAPI_BLOB,
 };
+
+type PARA = _CRYPTOAPI_BLOB;
+
+impl From<Vec<u8>> for PARA {
+    fn from(mut value: Vec<u8>) -> Self {
+        Self {
+            cbData: value.len() as c_uint,
+            pbData: value.as_mut_ptr(),
+        }
+    }
+}
 
 // https://cpdn.cryptopro.ru/content/capilite/html/group___store_func.html
 pub struct CertStore {
@@ -13,7 +24,8 @@ pub struct CertStore {
 }
 
 impl CertStore {
-    pub fn try_new<T: Into<Vec<u8>>>(store: T) -> Result<Self, ()> {
+    pub fn try_new<T: Into<String>>(store: T) -> Result<Self, ()> {
+        let store = store.into();
         let store = CString::new(store).unwrap();
         let store = store.as_ptr(); // as *const wchar_t;
 
@@ -48,7 +60,22 @@ impl CertStore {
 
     pub fn find_certificate_by_thumbprint<T: Into<String>>(&self, thumbprint: T) -> Result<(), ()> {
         let hash = unsafe { get_hash_from_hex(thumbprint)? };
+        let mut para: PARA = hash.into();
+        let para = &mut para as *mut _ as *const c_void;
 
+        let cert_context = unsafe {
+            CertFindCertificateInStore(
+                self.cert_store,
+                PKCS_7_ASN_ENCODING | X509_ASN_ENCODING,
+                0,
+                CERT_FIND_HASH,
+                para,
+                std::ptr::null(),
+            )
+        };
+        if cert_context.is_null() {
+            return Err(());
+        }
         todo!()
     }
 }
@@ -59,16 +86,19 @@ impl Drop for CertStore {
     }
 }
 
+/// ```
+/// let r = unsafe { get_hash_from_hex("046255290b0eb1cdd1797d9ab8c81f699e3687f3").unwrap() };
+/// assert_eq!(r, [4, 98, 85, 41, 11, 14, 177, 205, 209, 121, 125, 154, 184, 200, 31, 105, 158, 54, 135, 243]);
+/// ```
 unsafe fn get_hash_from_hex<T: Into<String>>(thumbprint: T) -> Result<Vec<u8>, ()> {
     let thumbprint = thumbprint.into();
     let thumbprint_len = thumbprint.len() as c_uint;
     let thumbprint = CString::new(thumbprint).unwrap();
-    let thumbprint = thumbprint.as_ptr(); // as *const wchar_t;
+    let thumbprint = thumbprint.as_ptr();
 
     let mut result_len: c_uint = 20;
     let mut result: Vec<u8> = Vec::with_capacity(result_len as usize);
     result.set_len(result_len as usize);
-    // dbg!(&result);
 
     let r = CryptStringToBinaryA(
         thumbprint,
@@ -79,48 +109,13 @@ unsafe fn get_hash_from_hex<T: Into<String>>(thumbprint: T) -> Result<Vec<u8>, (
         std::ptr::null_mut(),
         std::ptr::null_mut(),
     );
-    if r == 0 {
+    if r == 0 || result_len == 0 {
         return Err(());
-    };
+    }
 
-    // dbg!(&result);
-    // dbg!(&result_len);
     result.set_len(result_len as usize);
     Ok(result)
 }
-
-/*
-    HCERTSTORE hStoreHandle;
-    PCCERT_CONTEXT pCertContext = NULL;
-
-    BYTE pDest[20];
-    DWORD nOutLen = 20;
-
-    if (!CryptStringToBinary(thumbprint, 40, CRYPT_STRING_HEX, pDest, &nOutLen, 0, 0)) {
-        PyErr_SetString(PyExc_Exception, "CryptStringToBinary failed.");
-        return NULL;
-    }
-
-    CRYPT_HASH_BLOB para;
-    para.pbData = pDest;
-    para.cbData = nOutLen;
-
-    hStoreHandle = CertOpenSystemStore(0, storeName);
-    pCertContext = CertFindCertificateInStore(hStoreHandle, MY_ENCODING_TYPE, 0, CERT_FIND_HASH, &para, NULL);
-
-    if (!pCertContext) {
-        CertCloseStore(hStoreHandle, CERT_CLOSE_STORE_CHECK_FLAG);
-        PyErr_SetString(CertDoesNotExist, "Could not find the desired certificate.");
-        return NULL;
-    }
-
-    PyObject * certInfo = GetCertInfo(pCertContext);
-
-    CertFreeCertificateContext(pCertContext);
-    CertCloseStore(hStoreHandle, CERT_CLOSE_STORE_CHECK_FLAG);
-
-    return certInfo;
-*/
 
 #[cfg(test)]
 mod tests {
@@ -128,7 +123,7 @@ mod tests {
     use crate::CertStore;
 
     fn get_cert_store_my() -> CertStore {
-        CertStore::try_new("MY").unwrap()
+        CertStore::try_new("My").unwrap()
     }
 
     #[test]
@@ -145,6 +140,22 @@ mod tests {
     }
 
     #[test]
+    fn test_get_cert_by_thumbprint() {
+        let store = get_cert_store_my();
+        store
+            .find_certificate_by_thumbprint("046255290b0eb1cdd1797d9ab8c81f699e3687f3")
+            .unwrap();
+    }
+
+    #[test]
+    fn test_get_cert_by_thumbprint2() {
+        let store = get_cert_store_my();
+        store
+            .find_certificate_by_thumbprint("8cae88bbfd404a7a53630864f9033606e1dc45e2")
+            .unwrap();
+    }
+
+    #[test]
     fn test_get_hash_from_hex() {
         let r = unsafe { get_hash_from_hex("046255290b0eb1cdd1797d9ab8c81f699e3687f3").unwrap() };
         let lst = [
@@ -156,10 +167,8 @@ mod tests {
     }
 
     #[test]
-    fn test_get_cert_by_thumbprint() {
-        let store = get_cert_store_my();
-        store
-            .find_certificate_by_thumbprint("046255290b0eb1cdd1797d9ab8c81f699e3687f3")
-            .unwrap();
+    fn test_get_hash_from_hex2() {
+        let r = unsafe { get_hash_from_hex("0x046255290b0eb1cdd1797d9ab8c81f699e3687f3") };
+        assert!(r.is_err());
     }
 }
